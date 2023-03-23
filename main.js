e09f826bf55d17211f6c15f6aa53a211db2c404c
prohorov01 <alexprohorov01@gmail.com>
2023-03-23 13:27:30 +0200

    update finish

---
+++ main.js
@@ -0,0 +1,36 @@
+let action = prompt("Що ви хоче зробити (add, sub, mult, div)");
+if (action) {
+} else {
+  confirm("Шкода, що Ви не захотіли скористатися калькулятором");
+}
+
+const numberA = Number(prompt("Перше число"));
+const numberB = Number(prompt("Друге число"));
+
+switch (action) {
+  case "add":
+    alert(numberA + numberB);
+    console.log(add);
+    break;
+}
+switch (action) {
+  case "sub":
+    alert(numberA - numberB);
+    console.log(sub);
+    break;
+}
+switch (action) {
+  case "mult":
+    alert(numberA * numberB);
+    console.log(mult);
+    break;
+}
+switch (action) {
+  case "div":
+    alert(numberA / numberB);
+    console.log("div");
+    break;
+  default:
+    alert("Спробуйте ще раз");
+    break;
+}
